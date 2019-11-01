32d162bf92ee7b123ba30e34e9ddd6ce0665dfef
Andy Feidje Joseph <ingandy75@gmail.com>
2019-11-01 00:54:25 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,6 +1,7 @@
 module SongsHelper
 
     def display_artist song
+      if song.artist.present?
         link_to song.artist_name, song.artist
       else
         link_to 'Add Artist', edit_song_path(song)
