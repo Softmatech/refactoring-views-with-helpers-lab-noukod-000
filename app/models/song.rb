2b06c324217f9febe372958d01306076dd2d22b6
Andy Feidje Joseph <ingandy75@gmail.com>
2019-11-01 01:23:17 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name unless name.empty?)
+    self.artist = Artist.find_or_create_by(name: name unless name.empty?
   end
   
 end
